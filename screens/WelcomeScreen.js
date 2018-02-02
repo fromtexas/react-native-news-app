f90a892074a179e5976eac48ee5b5a4fb15de4ed
fromtexas <telures@yandex.ru>
2018-02-02 22:04:52 +0300

    viewpager container

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import {View, Text, ScrollView, StatusBar, ViewPagerAndroid, Dimensions, Image} from 'react-native';
 import {Icon, Button} from 'react-native-elements';
 import {connect} from 'react-redux';
-import {colorGreyDark1, colorGreyLight1} from '../assets/base';
+import {colorGreyDark1, colorGreyLight1, colorGreyLight2, colorGreyDark3, colorPrimary} from '../assets/base';
 import Select from '../components/Select/Select';
 import NavigationStateNotifier from '../utils/NavigationStateNotifier';
 import {addCategory, removeCategory} from '../actions/CategoryActions';
 import {addCountry, removeCountry} from '../actions/CountryActions';
 import {settingsUpdated} from '../actions/SettingsActions';
 import {submit} from '../utils';
+import Test from './test';
 
 const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
 
@@ -37,54 +38,69 @@ class WelcomeScreen extends Component{
     render () {
         
         return (
+                <Test ar={[1,2,3,4,56]}/>
+        )
+        
+        // return (
+        //     <View style={styles.screenContainer}>
+        //     <ViewPagerAndroid style={styles.viewPager}>
 
+        //     <View key='1'>
+        //     <Text style={{textAlign: 'center', color: '#fff'}}>1</Text>
+        //     </View>
 
-            <View style={styles.screenContainer}>
-                <ScrollView>
-                    <Text style={styles.screenTitle}>Get breaking news headlines with short description filtered by your interests and country preferences.</Text>
-                    <Text style={styles.screenParagraph}>{this.state.warning}</Text>
-                    <Icon
-                    name='settings'
-                    color={colorGreyLight1}
-                    size={40}
-                    containerStyle={styles.iconContainer}
-                    />
+        //     <View key='2'>
 
-                    <Text style={styles.screenParagraph}>Put your interests here: </Text>
-                    <Select
-                    add={this.props.addCategory}
-                    remove={this.props.removeCategory}
-                    items={this.props.category}
-                    style={{marginTop: 10, marginBottom: 10}}
-                    type='CATEGORY'
-                    options={categories}
-                    icon='call-received'
-                    />
+        //         <ScrollView>
+        //         <Text style={styles.screenTitle}>Get breaking news headlines with short description filtered by your interests and country preferences.</Text>
+        //         <Text style={styles.screenParagraph}>{this.state.warning}</Text>
+        //         <Icon
+        //         name='settings'
+        //         color={colorGreyLight1}
+        //         size={40}
+        //         containerStyle={styles.iconContainer}
+        //         />
 
-                    <Text style={styles.screenParagraph}>Select one or several countries: </Text>
-                    <Select
-                    add={this.props.addCountry}
-                    remove={this.props.removeCountry}
-                    items={this.props.country}
-                    style={{marginTop: 5, marginBottom: 20}}
-                    type='COUNTRY'
-                    options={country}
-                    icon='map'
-                    />
+        //         <Text style={styles.screenParagraph}>Put your interests here: </Text>
+        //         <Select
+        //         add={this.props.addCategory}
+        //         remove={this.props.removeCategory}
+        //         items={this.props.category}
+        //         style={{marginTop: 10, marginBottom: 10}}
+        //         type='CATEGORY'
+        //         options={categories}
+        //         icon='call-received'
+        //         />
 
-                    <Button 
-                    buttonStyle={{backgroundColor: colorGreyLight1, borderRadius: 3}}
-                    color={colorGreyDark1} 
-                    icon={{name: 'check', color: colorGreyDark1}} 
-                    title='DONE'
-                    onPress={() => submit(this.props.country.length, this.props.category.length, this, this.props.navigation.navigate)}
-                    />
-                </ScrollView>
+        //         <Text style={styles.screenParagraph}>Select one or several countries: </Text>
+        //         <Select
+        //         add={this.props.addCountry}
+        //         remove={this.props.removeCountry}
+        //         items={this.props.country}
+        //         style={{marginTop: 5, marginBottom: 20}}
+        //         type='COUNTRY'
+        //         options={country}
+        //         icon='map'
+        //         />
 
-                <Text style={{alignSelf: 'flex-end', color: colorGreyLight1, fontSize: 8}}>App was developed with newsapi.org</Text>
-            </View>
+        //         <Button 
+        //         buttonStyle={{backgroundColor: colorGreyLight1, borderRadius: 3}}
+        //         color={colorGreyDark1} 
+        //         icon={{name: 'check', color: colorGreyDark1}} 
+        //         title='DONE'
+        //         onPress={() => submit(this.props.country.length, this.props.category.length, this, this.props.navigation.navigate)}
+        //         />
+        //         </ScrollView>
 
-        );
+        //         <Text style={{alignSelf: 'flex-end', color: colorGreyLight1, fontSize: 8}}>App was developed with newsapi.org</Text>
+
+        //     </View>
+
+        //     </ViewPagerAndroid>
+                
+        //     </View>
+
+        // );
     }
 }
 
@@ -98,7 +114,7 @@ export default connect(mapStateToProps, {addCategory, removeCategory, addCountry
 
 const styles = {
     viewPager: {
-        height: SCREEN_HEIGHT
+        height: SCREEN_HEIGHT,
     },
     screenContainer: {
         flex: 1,
