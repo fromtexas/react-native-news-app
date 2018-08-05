46d9c4bcbd9e104359399110b51a474d2ebf2981
fromtexas <telures@yandex.ru>
2018-08-06 00:37:38 +0300

    comments removed

--- actions/newsActions.js
+++ actions/newsActions.js
@@ -12,20 +12,6 @@ export const fetchNews = () => async (dispatch, getState) => {
   const country = getState().country;
   let categoryArr = {};
 
-  // for(let i = 0; i < category.length; i++){
-
-  //   categoryArr[category[i]] = [];
-    
-  //   for (let j = 0; j < country.length; j++){
-  //     try {
-  //       let {data} = await axios.get(`${URL}${API_KEY}&category=${category[i]}&country=${country[j]}`);
-  //       categoryArr[category[i]] = [...categoryArr[category[i]], ...data.articles];
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   }
-  
-  // }
 
   const categoryKeys = Object.keys(category);
   const countryKeys = Object.keys(country);
